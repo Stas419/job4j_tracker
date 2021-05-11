d485a9d92e40bc8b41b109120e06e813fb392584
stanislav <1991>
2021-05-11 22:27:45 +0300

    1.1. Создание объекта [#405659]

--- src/main/java/ru/job4j/oop/Cat.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class Cat {
-    public static void main(String[] args) {
-        Cat polkan = new Cat();
-        Cat sharik = new Cat();
-        Cat zhychka = new Cat();
-    }
-}
