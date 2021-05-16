74e59f2f9324e93bbaf68ced4b69a1ec84b38ecb
stanislav <1991>
2021-05-16 22:17:12 +0300

    5. Аннотация @Override [#405673]

--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -1,6 +1,8 @@
 package ru.job4j.oop;
 
-public class JSONReport extends TextReport{
+public class JSONReport extends HtmlReport {
+
+    @Override
     public String generate(String name, String body) {
         return name + ": name," +  System.lineSeparator() + body + " : body";
     }
