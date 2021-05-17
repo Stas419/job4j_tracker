40159a2fac377cd7211b7cf3f4ee544a553879e5
stanislav <1991>
2021-05-17 13:32:11 +0300

    4. Переопределение [#405672]

--- src/main/java/ru/job4j/oop/HtmlReport.java
+++ src/main/java/ru/job4j/oop/HtmlReport.java
@@ -1,7 +1,6 @@
 package ru.job4j.oop;
 
 public class HtmlReport {
-
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -4,6 +4,7 @@ public class JSONReport extends HtmlReport {
 
     @Override
     public String generate(String name, String body) {
-        return name + ": name," +  System.lineSeparator() + body + " : body";
+        return "{ "+ System.lineSeparator() + " name : " + name +
+                System.lineSeparator() + " body : "+ body + System.lineSeparator() +"{ ";
     }
 }
