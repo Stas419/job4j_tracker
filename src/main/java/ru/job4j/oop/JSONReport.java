11e03f5ee2e4f671b64f8706a233b4dc27963fc5
stanislav <1991>
2021-05-17 21:19:12 +0300

    4. Переопределение [#405672]

--- src/main/java/ru/job4j/oop/JSONReport.java
+++ src/main/java/ru/job4j/oop/JSONReport.java
@@ -5,7 +5,7 @@ public class JSONReport extends HtmlReport {
     @Override
     public String generate(String name, String body) {
         return "{ "+ System.lineSeparator() + " name : " + name + ","+
-                System.lineSeparator() + " body : "+ body + System.lineSeparator() +"{ ";
+                System.lineSeparator() + " body : "+ body + System.lineSeparator() +"} ";
     }
 
     public static void main(String[] args) {
