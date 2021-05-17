eaa581fd07017249a593c8f4dcf4778095480b31
stanislav <1991>
2021-05-17 21:25:04 +0300

    2. Модель данных. [#405679]

--- src/main/java/ru/job4j/pojo/Student.java
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -7,40 +7,20 @@ public class Student {
     private int group;
     private double dateRceipts;
 
-    public String getSurname() {
-        return surname;
-    }
-
     public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getPatronymic() {
-        return patronymic;
-    }
-
     public void setPatronymic(String patronymic) {
         this.patronymic = patronymic;
     }
 
-    public int getGroup() {
-        return group;
-    }
-
     public void setGroup(int group) {
         this.group = group;
-    }
-
-    public double getDateRceipts() {
-        return dateRceipts;
     }
 
     public void setDateRceipts(double dateRceipts) {
