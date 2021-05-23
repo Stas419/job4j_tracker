e115f135c6f8157e42ca27ec74e0a2dbaa19f9cb
stanislav <1991>
2021-05-23 09:01:44 +0300

    2.3. Реализация класса StartUI. Добавление заявки [#500744]

--- src/main/java/ru/job4j/tracker/StartUITwo.java
+++ src/main/java/ru/job4j/tracker/StartUITwo.java
@@ -4,54 +4,33 @@ import java.util.Scanner;
 
 public class StartUITwo {
     public void init(Scanner scanner, Tracker tracker) {
-        boolean run = false;
+        boolean run = true;
         while (run) {
-            this.showMenu();
+            showMenu();
             System.out.print("Select: ");
-            int select = Integer.valueOf(scanner.nextLine());
-            if (select == 0) {
+            int select = Integer.parseInt(scanner.nextLine());
+            if(select == 0){
                 System.out.println("=== Create a new Item ====");
                 System.out.print("Enter name: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-                if(select == 1){
-                    Item[] item2 = new Item[10];
-                    Item[] list = tracker.findAll(item2);
-                        System.out.println(list.toString());
-                }
-
-                if(select == 2){
-                    System.out.println("введите id который хотите изменить");
-                    int id = Integer.parseInt(scanner.nextLine());
-                    tracker.findById(id);
-                    Item item1 = new Item();
-                    tracker.replace(item1, id);
-                }
-
-                if(select == 3){
-
-                }
-
-
-                /*            } else if (...) { */
-                /*             Добавить остальные действия системы по меню. */
-            } else if (select == 6) {
+                System.out.println("Добавленная заявка: " + item);
+            }
+            else if(select == 6){
                 run = false;
             }
         }
     }
 
     private void showMenu() {
-        System.out.println("Menu." +
-                "0. Add new Item\n" +
-                "        1. Show all items\n" +
-                "        2. Edit item\n" +
-                "        3. Delete item\n" +
-                "        4. Find item by Id\n" +
-                "        5. Find items by name\n" +
-                "        6. Exit Program\n" +
-                "        Select:");
+        String[] menu = {"Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"};
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + "." + menu[i]);
+        }
 
     }
 
