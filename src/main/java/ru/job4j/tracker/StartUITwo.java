1670a0aab33fe3973895b03b262c6142220c66d2
stanislav <1991>
2021-05-26 08:27:57 +0300

    2.1. Реализация класса StartUI [#784 #159153]

--- src/main/java/ru/job4j/tracker/StartUITwo.java
+++ src/main/java/ru/job4j/tracker/StartUITwo.java
@@ -88,7 +88,6 @@ public class StartUITwo {
         }
     }
 
-
     private void showMenu() {
         String[] menu = {"Add new Item", "Show all items", "Edit item",
                 "Delete item", "Find item by id", "Find items by name",
