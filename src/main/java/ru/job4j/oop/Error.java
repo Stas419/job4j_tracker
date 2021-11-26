ff51144e8540794559d4c5894d9fd4268066a7a0
stanislav <1991>
2021-11-26 14:30:34 +0300

    1.7. Конструктор [#173889 #115585]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -1,32 +1,33 @@
 package ru.job4j.oop;
 
 public class Error {
-    private boolean active;
+    private  boolean active;
     private int status;
     private String message;
 
     public Error() {
+
     }
 
     public Error(boolean active, int status, String message) {
         this.active = active;
-        this.status =  status;
+        this.status = status;
         this.message = message;
-
     }
 
-    public void vivod() {
+    public void errorInfo() {
         System.out.println(active);
         System.out.println(status);
         System.out.println(message);
     }
 
     public static void main(String[] args) {
-        Error error = new Error(true, 16, "добро пожаловать;");
-        Error error2 = new Error(false, 10, "извините;");
+        Error error = new Error();
+        Error error2 = new Error(true, 1, "не значительная");
+        Error error3 = new Error(false, 0, "ошибка не обнаружена");
 
-        error.vivod();
-        error2.vivod();
+        error.errorInfo();
+        error2.errorInfo();
+        error.errorInfo();
     }
-
 }
