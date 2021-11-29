7620426991c0ee96b803ae906cbe874f97ec0f94
stanislav <1991>
2021-11-29 22:06:30 +0300

    3. Массивы и модели. [#174892 #115603]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -2,34 +2,45 @@ package ru.job4j.pojo;
 
 public class Library {
     public static void main(String[] args) {
-        Book cleanCcode = new Book("Чистый код", 250);
+        Book cleanCcode = new Book("Clean code", 250);
         Book literature = new Book("Литература", 230);
         Book geography = new Book("География", 100);
         Book physics = new Book("физика", 150);
 
-        Book[] book = new Book[4];
-        book[0] = cleanCcode;
-        book[1] = literature;
-        book[2] = geography;
-        book[3] = physics;
+        Book[] mas = new Book[4];
+        mas[0] = cleanCcode;
+        mas[1] = literature;
+        mas[2] = geography;
+        mas[3] = physics;
 
-        for (Book bok: book) {
-            System.out.println(bok.getName() + " - " + bok.getNumPages());
+        for (int i = 0; i < mas.length; i++) {
+            Book bok = mas[i];
+
+            System.out.println(i + "Название: " + bok.getName() + "; " + " Kоличество страниц: "
+                    + bok.getNumPages());
         }
 
-        Book bok = book[0];
-        book[0] = book[3];
-        book[3] = bok;
-        System.out.println("ячейка с индексом 0 - " + book[0].getName()
-                + " - " +  book[0].getNumPages());
-        System.out.println("ячейка с индексом 3 - " + book[3].getName()
-                + " - " +  book[3].getNumPages());
+            Book replacement =  mas[0];
+            mas[0] = mas[3];
+            mas[3] = replacement;
 
-        for (Book bok1: book) {
-            if ("Чистый код".equals(bok1.getName())) {
-                System.out.println("В списке есть такая книга");
-                System.out.println(bok.getName() + " - " + bok.getNumPages());
+            System.out.println();
+
+            for (int j = 0; j < mas.length; j++) {
+                Book book = mas[j];
+                System.out.println(j + "Название: " + book.getName() + "; "
+                        + " Kоличество страниц: "
+                        + book.getNumPages());
             }
+        System.out.println();
+
+        for (int i = 0; i < mas.length; i++) {
+            Book search = mas[i];
+            if (search.getName() == "Clean code") {
+                System.out.println("книга найдена: " + search.getName());
+                break;
+            }
+
         }
     }
 }
