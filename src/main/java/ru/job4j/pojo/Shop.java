0b23102ffa023bca4ec040f46a0f071554594e11
stanislav <1991>
2021-11-30 21:25:15 +0300

    3.1. Массив с пустыми ячейками [#176470 #115604]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -7,6 +7,7 @@ public class Shop {
         for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
                 num = i;
+                break;
             }
         }
         return num;
