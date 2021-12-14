d5adf50495e7f946c9e4daebad0925f62565e91f
stanislav <1991>
2021-12-14 23:08:53 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,29 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends java.lang.Exception {
+    public ElementNotFoundException(String name) {
+        super(name);
+    }
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                throw new ElementNotFoundException("Url could not be null");
+            } else {
+                new ElementNotFoundException("Url could not be null");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] mas = new String[]{"окно", "молоко", "трамвай"};
+        try {
+            indexOf(mas, "окно");
+
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
