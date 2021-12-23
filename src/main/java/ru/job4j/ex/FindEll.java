54d9718a1cea94ed4837007d4452ca67f2f2685d
stanislav <1991>
2021-12-23 12:58:29 +0300

    0.4. Пользовательские исключения. [#219367 #240146]

---
+++ src/main/java/ru/job4j/ex/FindEll.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+import static ru.job4j.ex.UserStore.findUser;
+import static ru.job4j.ex.UserStore.validate;
+
+public class FindEll {
+    public static int indexOf(String[] values, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw  new ElementNotFoundException("Url could not be null");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] values = new String[]{"Петр", "Семен"};
+        String key = "Петр";
+        indexOf(values, key);
+    }
+}
