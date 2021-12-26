353b904dc039d601bd9f39ab007b036f0384f6f6
stanislav <1991>
2021-12-26 21:36:18 +0300

    1.1. Тесты на StartUI. [#223313 #243765]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -50,7 +50,7 @@ public class StartUITest {
 
     @Test
     public void whenDeleteItem() {
-        Output out = new Output() {
+        Output out = new StubOutput() {
             @Override
             public void println(Object obj) {
 
@@ -67,6 +67,29 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
+    }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                    new String[]{
+                            "111", "0" }
+                );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu." + ln
+                        + "0. Exit Program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit Program" + ln
+                )
+        );
     }
 
 }
