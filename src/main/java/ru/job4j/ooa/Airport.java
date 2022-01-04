ce3a22603a21640faafb22a2c2aaafdc8991637b
stanislav <1991>
2022-01-04 22:27:37 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,15 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+        Airbus airbus2 = new Airbus("A380");
+        System.out.println(airbus2);
+        airbus2.printModel();
+        airbus2.printCountEngine();
+    }
+}
